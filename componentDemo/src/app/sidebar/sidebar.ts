fb0a6e7116beba0f4dc21f9afe7677634b982f02
Unggul Dwi Utomo <unggul.utomo@kemenkeu.go.id>
2025-07-22 15:55:27 +0700

    [styling] styliing sidebar, navbar dan content dengan angular material

--- componentDemo/src/app/sidebar/sidebar.ts
+++ componentDemo/src/app/sidebar/sidebar.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatNavList } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatSidenavModule, MatToolbarModule],
+  imports: [MatSidenavModule, MatToolbarModule, MatNavList, MatIconModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
